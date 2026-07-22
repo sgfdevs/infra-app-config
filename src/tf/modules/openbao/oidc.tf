bf9331a3baa967a9b475e7d5fadd3fd1703b4f88
Levi Zitting <me@levizitting.com>
2026-07-22 09:55:19 -0700

    fix: allow oidc users to manage secrets

--- src/tf/modules/openbao/oidc.tf
+++ src/tf/modules/openbao/oidc.tf
@@ -5,6 +5,35 @@ ephemeral "aws_ssm_parameter" "dex_client_secrets" {
   arn = "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/vm-workloads/sgfdevs/infra-vm-workloads/dex-client-secrets"
 }
 
+resource "vault_policy" "application_secrets_admin" {
+  name   = "application-secrets-admin"
+  policy = <<-EOT
+    path "${vault_mount.applications.path}/data/*" {
+      capabilities = ["create", "read", "update", "delete", "patch"]
+    }
+
+    path "${vault_mount.applications.path}/metadata" {
+      capabilities = ["list"]
+    }
+
+    path "${vault_mount.applications.path}/metadata/*" {
+      capabilities = ["create", "read", "update", "delete", "list"]
+    }
+
+    path "${vault_mount.applications.path}/delete/*" {
+      capabilities = ["update"]
+    }
+
+    path "${vault_mount.applications.path}/undelete/*" {
+      capabilities = ["update"]
+    }
+
+    path "${vault_mount.applications.path}/destroy/*" {
+      capabilities = ["update"]
+    }
+  EOT
+}
+
 resource "vault_jwt_auth_backend" "oidc" {
   description                   = "OIDC auth via SGF Devs Dex (dex.sgf.dev)"
   path                          = "oidc"
@@ -25,7 +54,7 @@ resource "vault_jwt_auth_backend_role" "admin" {
   bound_claims = {
     groups = "sgfdevs:platform-admins"
   }
-  token_policies = ["default"]
+  token_policies = ["default", vault_policy.application_secrets_admin.name]
   oidc_scopes    = ["openid", "profile", "email", "groups"]
   allowed_redirect_uris = [
     "https://secrets.sgf.dev/ui/vault/auth/oidc/oidc/callback",
